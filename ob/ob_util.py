6833e07ce9887c0bb697edee477884b71066f2b8
assaf <asssaf@localhost>
2017-06-21 17:41:31 -0700

    add find syllable by pattern

--- ob/ob_util.py
+++ ob/ob_util.py
@@ -1,8 +1,35 @@
+import ob
+
+from itertools import ifilter
+
+
 def nth_planet_of_star(star, n):
 	return n * 65536 + star
+
 
 def generate_planets(star):
 	planet_index = 1
 	while planet_index < 65536:
 		yield nth_planet_of_star(star, planet_index)
 		planet_index += 1
+
+def find_syllable(set, pattern):
+	if pattern.startswith('*'):
+		pattern = pattern[1:]
+		syllables = (ob.get_syllable(set, i) for i in xrange(0, 0x100))
+		syllables = ifilter(lambda x: x.endswith(pattern), syllables)
+		return syllables
+
+	elif pattern.endswith('*'):
+		pattern = pattern[:-1]
+		syllables = (ob.get_syllable(set, i) for i in xrange(0, 0x100))
+		syllables = ifilter(lambda x: x.startswith(pattern), syllables)
+		return syllables
+
+
+def find_prefix_syllable(pattern):
+    return find_syllable(ob.prefix, pattern)
+
+
+def find_suffix_syllable(pattern):
+    return find_syllable(ob.suffix, pattern)
--- ob/test_ob_util.py
+++ ob/test_ob_util.py
@@ -11,3 +11,37 @@ class ObUtilTest(unittest.TestCase):
         planets = ob_util.generate_planets(0x4141)
         self.assertEquals(planets.next(), 0x14141)
         self.assertEquals(planets.next(), 0x24141)
+
+    def test_find_prefix_syllable(self):
+        results = ob_util.find_prefix_syllable('*')
+        self.assertEquals(results.next(), 'doz')
+        self.assertEquals(results.next(), 'mar')
+
+        results = ob_util.find_prefix_syllable('n*')
+        self.assertEquals(results.next(), 'nar')
+        self.assertEquals(results.next(), 'nov')
+
+        results = ob_util.find_prefix_syllable('*n')
+        self.assertEquals(results.next(), 'bin')
+        self.assertEquals(results.next(), 'wan')
+
+        results = ob_util.find_prefix_syllable('*in')
+        self.assertEquals(results.next(), 'bin')
+        self.assertEquals(results.next(), 'rin')
+
+    def test_find_suffix_syllable(self):
+        results = ob_util.find_suffix_syllable('*')
+        self.assertEquals(results.next(), 'zod')
+        self.assertEquals(results.next(), 'nec')
+
+        results = ob_util.find_suffix_syllable('n*')
+        self.assertEquals(results.next(), 'nec')
+        self.assertEquals(results.next(), 'nup')
+
+        results = ob_util.find_suffix_syllable('*n')
+        self.assertEquals(results.next(), 'pen')
+        self.assertEquals(results.next(), 'sun')
+
+        results = ob_util.find_suffix_syllable('*yn')
+        self.assertEquals(results.next(), 'byn')
+        self.assertEquals(results.next(), 'syn')
