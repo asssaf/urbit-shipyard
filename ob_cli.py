b1a0e5ce2bf2e00039a105a25d00e04c87a0ef89
assaf <asssaf@localhost>
2017-06-24 14:20:10 -0700

    add filtering by star

--- ob_cli.py
+++ ob_cli.py
@@ -24,61 +24,75 @@ def print_star_and_planet(planet):
     print("0x%04x %s: %s" % (star, star_name, planet_name))
 
 
-def find_planet_with_prefixes(galaxy, words):
+def find_planet_with_prefixes(galaxy, star, words):
     for prefix in words:
-        find_planet_with_prefix(galaxy, prefix)
+        find_planet_with_prefix(galaxy, star, prefix)
 
 
-def find_planet_with_prefix(galaxy, prefix):
+def find_planet_with_prefix(galaxy, star, prefix):
     addr = ob.from_ship_name(prefix) * 0x10000
 
     planet_range = (addr + i for i in xrange(0x1, 0x10000))
-    planets = find_planet(planet_range, galaxy)
+    planets = find_planet(planet_range, galaxy, star)
 
     for planet in planets:
         print_star_and_planet(planet)
 
 
-def find_planet_with_suffixes(galaxy, words):
+def find_planet_with_suffixes(galaxy, star, words):
     for suffix in words:
-        find_planet_with_suffix(galaxy, suffix)
+        find_planet_with_suffix(galaxy, star, suffix)
 
 
-def find_planet_with_suffix(galaxy, suffix):
+def find_planet_with_suffix(galaxy, star, suffix):
     addr = ob.from_ship_name(suffix)
 
     planet_range = (addr + i*0x10000 for i in xrange(0x1, 0x10000))
-    planets = find_planet(planet_range, galaxy)
+    planets = find_planet(planet_range, galaxy, star)
 
     for planet in planets:
         print_star_and_planet(planet)
 
 
-def find_planet_with_double(galaxy):
+def find_planet_with_double(galaxy, star):
     # increment both low word and high word so by 0x10001
     planet_range = (i*0x10001 for i in xrange(0x1, 0x10000))
-    planets = find_planet(planet_range, galaxy)
+    planets = find_planet(planet_range, galaxy, star)
 
     for planet in planets:
         print_star_and_planet(planet)
 
 
-def find_planet_with_both(galaxy, words):
+def find_planet_with_both(galaxy, star, words):
     # generate all word combinations from the word list
     combinations = combinations_with_replacement(words, 2)
-    for c in combinations:
-        name = "%s-%s" % c
-        address = ob.from_ship_name(name)
-        if address & 0xff == galaxy and address & 0xff00 != 0:
-            print_star_and_planet(address)
+    planets = (ob.from_ship_name("%s-%s" % c) for c in combinations)
+    planets = filter_by_galaxy(galaxy, planets)
+    planets = filter_by_star(star, planets)
+
+    for planet in planets:
+        print_star_and_planet(planet)
 
 
-def find_planet(addr_range, galaxy):
+def filter_by_galaxy(galaxy, planets):
     galaxy_pred = lambda x: x & 0xff == galaxy
-    star_pred = lambda x: x & 0xff00 != 0
+    return ifilter(galaxy_pred, planets)
+
+
+def filter_by_star(star, planets):
+    if star:
+        star_pred = lambda x: x & 0xff00 == star & 0xff00
+
+    else:
+        star_pred = lambda x: x & 0xff00 != 0
+
+    return ifilter(star_pred, planets)
+
+
+def find_planet(addr_range, galaxy, star=None):
     planets = (ob.fend(i) for i in addr_range)
-    planets = ifilter(galaxy_pred, planets)
-    planets = ifilter(star_pred, planets)
+    planets = filter_by_galaxy(galaxy, planets)
+    planets = filter_by_star(star, planets)
 
     return planets
 
@@ -86,6 +100,7 @@ def find_planet(addr_range, galaxy):
 def find_words(prefix, suffix):
     for word in ob_util.find_words(prefix, suffix):
         print(word)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -109,6 +124,7 @@ if __name__ == '__main__':
 
     planet_parser = subparsers.add_parser('planet', help='find planet by partial name')
     planet_parser.add_argument('-g', '--galaxy', default='0x0', help='galaxy to search in')
+    planet_parser.add_argument('-s', '--star', help='star to search in')
     planet_parser.add_argument('--type', choices=['prefix', 'suffix', 'both', 'double', 'star'], help='search type')
     planet_parser.add_argument('name', help='name to search for', nargs='+')
 
@@ -134,19 +150,23 @@ if __name__ == '__main__':
 
     elif args.command == 'planet':
         galaxy = int(args.galaxy, 16)
+        star = None
+        if args.star:
+            star = int(args.star, 16)
+
         search_type = args.type
 
         if search_type == 'prefix':
-            find_planet_with_prefixes(galaxy, args.name)
+            find_planet_with_prefixes(galaxy, star, args.name)
 
         elif search_type == 'suffix':
-            find_planet_with_suffixes(galaxy, args.name)
+            find_planet_with_suffixes(galaxy, star, args.name)
 
         elif search_type == 'both':
-            find_planet_with_both(galaxy, args.name)
+            find_planet_with_both(galaxy, star, args.name)
 
         elif search_type == 'double':
-            find_planet_with_double(galaxy)
+            find_planet_with_double(galaxy, star)
 
         elif search_type == 'star':
             star = ob.from_ship_name(args.name, 2)
